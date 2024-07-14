620191bd57ed3ef2da974ab573e1576dbe9a46e7
sota <hitoesouta@gmail.com>
2024-07-14 23:38:51 +0900

    add calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import {title} from "@/app/index.css";
+'use client'
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import {calendar, title} from "@/app/index.css";
 
 export default function Home() {
   return (
     <main>
       <h1 className={title}>HOME</h1>
+      <div className={calendar}>
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth"/>
+      </div>
+      <button>
+        <h2>本日のトレーニングを追加</h2>
+      </button>
     </main>
-  );
+  )
 }
