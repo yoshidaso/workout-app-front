98d9e6cd54104b683ab26872b364ed00bc2dedaa
sota <hitoesouta@gmail.com>
2024-08-04 23:56:45 +0900

    tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,25 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {calendar, title} from "@/app/index.css";
 
-interface workDetail{
+type WorkDetail = {
   title: string;
-  start: string
+  start: string;
 }
 
+const workDetails: WorkDetail[] = [
+  {title: 'chest', start: '2024-08-01'}
+];
+
 export default function Home() {
-  const workDetails : workDetail[] = []
   return (
     <main>
       <h1 className={title}>HOME</h1>
       <div className={calendar}>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" events={workDetails}/>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          events={workDetails}
+        />
       </div>
       <button>
         <h2>本日のトレーニングを追加</h2>
